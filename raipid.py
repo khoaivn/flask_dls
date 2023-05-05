d3cefc254b32a33e0b9cb9bc91369ba6070b3cee
phamgi <trungdunght2003@gmail.com>
2023-05-05 16:52:02 +0700

    aaa

--- raipid.py
+++ raipid.py
@@ -1,13 +1,26 @@
+from flask import Flask, render_template
 import requests
 
-url = "https://iata-and-icao-codes.p.rapidapi.com/airlines"
+app = Flask(__name__)
 
-headers = {
-	"content-type": "application/octet-stream",
-	"X-RapidAPI-Key": "d91fb1e8a3msh22ab32c37f06602p1e9875jsnc5aefe1fe85d",
-	"X-RapidAPI-Host": "iata-and-icao-codes.p.rapidapi.com"
-}
+@app.route('/')
 
-response = requests.get(url, headers=headers)
+def index():
+	name = "Pham Trung Dung"
+	response = get_json()
+	return render_template('index.html', name = name, response = response.json)
 
-print(response.json())
+def get_json():
+	url = "https://iata-and-icao-codes.p.rapidapi.com/airlines"
+    
+	headers = {
+		"content-type": "application/octet-stream",
+		"X-RapidAPI-Key": "d91fb1e8a3msh22ab32c37f06602p1e9875jsnc5aefe1fe85d",
+		"X-RapidAPI-Host": "iata-and-icao-codes.p.rapidapi.com"
+	}
+
+	response = requests.get(url, headers=headers)
+	return response
+
+if __name__ == '__main__':
+	app.run(debug=True)
