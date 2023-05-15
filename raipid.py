af506d2910c30f1b393844c21ad3432da0bc91e4
phamgi <trungdunght2003@gmail.com>
2023-05-15 16:46:41 +0700

    use for if in html

--- raipid.py
+++ raipid.py
@@ -8,12 +8,27 @@ app = Flask(__name__)
 
 def index():
 	name = "Pham Trung Dung"
-	response = get_json()
-	len_json = len(response.json())
+	response = get_json_flight_area()
+	len_json = len(response.json()['flights'])
 
 
 	return render_template('index.html', name = name, response = response.json(), len_json = len_json)
 	# return render_template('index_one_flight.html', name = name, response = response.json)
+
+def get_json_flight_area():
+
+	url = "https://flight-data4.p.rapidapi.com/get_area_flights"
+
+	querystring = {"latitude":"21.0278","longitude":"105.8342"}
+
+	headers = {
+		"X-RapidAPI-Key": "fa97e59c65mshccb0c63302d8440p13c575jsn4742b0fd94a2",
+		"X-RapidAPI-Host": "flight-data4.p.rapidapi.com"
+	}
+
+	response = requests.get(url, headers=headers, params=querystring)
+
+	return response
 
 def get_json():
 	url = "https://iata-and-icao-codes.p.rapidapi.com/airlines"
